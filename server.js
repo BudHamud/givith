1abdd86d8c3ee5d0f915c33efe6a5a84e7f3f191
budhamud <nigitart@gmail.com>
2022-11-14 13:54:34 -0300

    subiendo progreso

--- server.js
+++ server.js
@@ -1,47 +1,40 @@
-const express = require("express");
-// const moment = require('moment')
-const fs = require("fs/promises");
-const app = express();
-const PORT = 8080;
-const classContenedor = require('./container/Contenedor')
+const express = require('express')
+const app = express()
 
-// Provando moment.js
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+const PORT = 8080
 
-// const visitas = {
-//   productos: 0,
-//   fecha_ingreso: '',
-//   random: 0,
-//   fecha_ingreso_random: ''
-// }
+app.get('/api/mensajes', (req, res) => {
+    console.log('HTTP GET')
+    res.json({msg: 'hola mundo'})
+})
 
-const productos = new classContenedor("./products.json");
+app.get('/api/mensajes-query-params', (req, res) => {
+  console.log('GET');
+  if (Object.entries(req.query).length > 0) {
+    res.json ({
+      res: 'get ok',
+      query: req.query
+    })
+  } else {
+    res.json({
+      result: 'no se envió'
+    })
+  }
+})
 
-app.get("/", (req, ans) => {
-  ans.send(`<h1 style="color: #666">Bienvenido</h1>`);
-});
+app.get('/api/mensajes/:id', (req, res) => {
+  res.json({
+    result: 'recurso',
+    id: req.params.id
+  })
+})
 
-app.get("/productos", async (req, ans) => {
-  // visitas.productos++
-  // visitas.fecha_ingreso = moment().format('MMMM Do YYYY, h:mm:ss a')
-  const prods = await productos.getAll()
-  ans.send(prods);
-});
+app.post('/api/mensajes/', (req, res) => {
+  res.json({msg: 'hola mundo'})
+})
 
-app.get('/random', async (req, ans) => {
-  // visitas.random++
-  // visitas.fecha_ingreso_random = moment().format('MMMM Do YYYY, h:mm:ss a')
-  const prods = await productos.getAll()
-  const number = Math.ceil(Math.random()*prods.length - 1)
-  const getRandom = prods[number]
-  ans.send(getRandom)
-});
-
-// app.get('/visitas', (req, ans) => {
-//   ans.send({ visitas })
-// })
-
-// Puerto
-
-const server = app.listen(PORT, () => {
-  console.log(`Servidor express en el punto: ${PORT}`);
-});
+app.listen(PORT, () => {
+    console.log('Server listo');
+})
