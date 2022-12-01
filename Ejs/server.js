4de09197880e0c4f9358c2c97fad1655d5c126be
budhamud <nigitart@gmail.com>
2022-12-01 03:31:23 -0300

    merge quintaEntrega

---
+++ Ejs/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+
+const productos = []
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+const PORT = 8080
+
+app.get('/', (req, res) => {
+  res.render('Inicio', { productos })
+})
+
+app.post('/productos', (req, res) => {
+  productos.push(req.body)
+  res.redirect('/')
+})
+
+app.listen(PORT, () => {
+  console.log('200 OK ' + PORT);
+})
